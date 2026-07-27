2d09c0cae683c27000c09f5c146dda26aa440d59
AlexandraWong22054 <27ycr7cs69@privaterelay.appleid.com>
2026-07-27 14:35:45 +1200

    Added Continue and Name Text

--- line_edit.gd
+++ line_edit.gd
@@ -1,13 +1,17 @@
 extends LineEdit
 
 var player_name : String = ""
+@onready var Continue = get_node("Continue")
+@onready var Greeting = get_node("Greeting")
 
 func _ready():
 	max_length = 15 #only 15 character limit
-	
+
+
 
 #Changes Player name to the submitted text
 func _on_text_submitted(new_text: String):
 	player_name = new_text
 	print("Name = ", player_name)
-	
+	Continue.show() #Shows the "Click to Continue" button
+	Greeting.text = (str("Hello, ") + (player_name))

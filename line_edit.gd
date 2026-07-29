ed02ff03b425651eaf9a44d4f091d4b3f1f34978
AlexandraWong22054 <27ycr7cs69@privaterelay.appleid.com>
2026-07-29 12:48:53 +1200

    Added pop_up for No Name Input
    Pop-up window appears when user presses enter without any letters in the input_box. Disappears when x is pressed.

--- line_edit.gd
+++ line_edit.gd
@@ -3,15 +3,24 @@ extends LineEdit
 var player_name : String = ""
 @onready var Continue = get_node("Continue")
 @onready var Greeting = get_node("Greeting")
+@onready var Noname = get_node("No_Input")
+
 
 func _ready():
 	max_length = 15 #only 15 character limit
-
-
+	Noname.hide()
 
 #Changes Player name to the submitted text
 func _on_text_submitted(new_text: String):
-	player_name = new_text
-	print("Name = ", player_name)
-	Continue.show() #Shows the "Click to Continue" button
-	Greeting.text = (str("Hello, ") + (player_name))
+	if new_text.is_empty():
+		print("Test")
+		Noname.show()
+	else:
+		player_name = new_text
+		print("Name = ", player_name)
+		Continue.show() #Shows the "Click to Continue" button
+		Greeting.text = (str("Hello, ") + (player_name))
+
+
+func _on_no_input_close_requested():
+	Noname.hide()

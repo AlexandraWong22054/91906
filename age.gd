c33ebfb43359d14e39868ab01e31c22088f009cb
AlexandraWong22054 <27ycr7cs69@privaterelay.appleid.com>
2026-08-12 13:39:34 +1200

    Fixed Age.Gd according to gdstyle checker suggestions

--- age.gd
+++ age.gd
@@ -1,69 +1,69 @@
 extends LineEdit
 
 var player_age : String = ""
-var regexL: = RegEx.new()
-var regexSC = RegEx.new()
-@onready var Invalid = get_node("Invalid_Input")
-@onready var Age = get_node("Invalid_Input/AgeError")
-@onready var Space = get_node("Invalid_Input/SpaceError")
-@onready var Letter = get_node("Invalid_Input/LetterError")
-@onready var SpecChar = get_node("Invalid_Input/SpecialCharError")
-@onready var Old = get_node("Invalid_Input/OldestError")
-@onready var Young = get_node("Invalid_Input/ChildError")
-@onready var Continue = get_node("StartGame")
+var regex_l: = RegEx.new()
+var regex_sc = RegEx.new()
+@onready var invalid = get_node("Invalid_Input")
+@onready var age = get_node("Invalid_Input/AgeError")
+@onready var space = get_node("Invalid_Input/SpaceError")
+@onready var letter = get_node("Invalid_Input/LetterError")
+@onready var spec_char = get_node("Invalid_Input/SpecialCharError")
+@onready var old = get_node("Invalid_Input/OldestError")
+@onready var young = get_node("Invalid_Input/ChildError")
+@onready var proceed = get_node("StartGame")
 
 func _ready():
-	regexL.compile(r"[a-zA-Z]") #compiles the alphabet to later check if the input includes a letter.
-	regexSC.compile(r"[^\w\s]")
+	regex_l.compile(r"[a-zA-Z]") #compiles the alphabet
+	regex_sc.compile(r"[^\w\s]") #compiles special characters
 func _on_text_changed(new_age):
-	var search = regexL.search(new_age) #searches if the input is in the regex compile
-	var search2 = regexSC.search(new_age)
+	var search = regex_l.search(new_age) #searches if the input is in the regex compile
+	var search2 = regex_sc.search(new_age)
 	if " " in new_age:
 		var cursor_place = caret_column #Save cursor
 		text = new_age.replace(" ","") #Replace space with nothing, removes the space
 		caret_column = cursor_place -1 #Reset cursor to saved
-		Invalid.show() 
-		Space.show()
-	if search:
-		print("Found letter")
+		invalid.show() 
+		space.show()
+	if search: #if a letter is found, do the following:
+		print("Found letter") 
 		var cursor_place = caret_column
-		var replacedage = regexL.sub(new_age, "",)
+		var replacedage = regex_l.sub(new_age, "",)
 		text = replacedage
 		caret_column = cursor_place -1
-		Invalid.show()
-		Letter.show()
-	if search2:
+		invalid.show()
+		letter.show()
+	if search2: #if a special character is found, do the following: 
 		print("Found Specchar")
 		var cursor_place = caret_column
-		var replacedage = regexSC.sub(new_age, "",)
+		var replacedage = regex_sc.sub(new_age, "",)
 		text = replacedage
 		caret_column = cursor_place -1
-		Invalid.show()
-		SpecChar.show()
+		invalid.show()
+		spec_char.show()
 
 func _on_text_submitted(new_age):
 	var int_age = int(new_age)
 	if new_age.is_empty():
-		Invalid.show()
-		Age.show()
+		invalid.show()
+		age.show()
 	elif int_age > 123: #Error popup if age is over maximum
-		Invalid.show()
-		Old.show()
+		invalid.show()
+		old.show()
 	elif int_age < 10 and int_age > -1:
-		Invalid.show() #Error popup if age is under minimum
-		Young.show()
+		invalid.show() #Error popup if age is under minimum
+		young.show()
 	else:
 		editable = false
 		player_age = new_age
 		print("Age = ", player_age)
-		Continue.show()
+		proceed.show()
 
 func _on_invalid_input_close_requested():
-	Invalid.hide()
-	Age.hide()
-	Space.hide()
-	Old.hide()
-	Young.hide()
-	Letter.hide()
-	SpecChar.hide()
+	invalid.hide()
+	age.hide()
+	space.hide()
+	old.hide()
+	young.hide()
+	letter.hide()
+	spec_char.hide()
 #hides the messages in the pop up once closed so that new messages won't overlap
